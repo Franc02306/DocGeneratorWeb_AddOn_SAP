3a8bbfb86fc5743450f67c8401a757359ece1b14
Franc02306 <francosm230602@gmail.com>
2026-05-31 11:11:27 -0500

    [feat] implementando icono principal en menu de AddOn

--- AddOn_DocGenerator.UL/Menus/MenuLoader.cs
+++ AddOn_DocGenerator.UL/Menus/MenuLoader.cs
@@ -8,6 +8,9 @@ namespace AddOn_DocGenerator.UL.Menus
 {
     public class MenuLoader
     {
+        private const string MAIN_MENU_UID = "DOCGEN_MAIN";
+        private const string GENERATOR_MENU_UID = "DOCGEN_GENERATOR";
+
         /// <summary>
         /// Carga el menú personalizado del AddOn desde el archivo XML y lo registra en SAP Business One.
         /// </summary>
@@ -15,20 +18,28 @@ namespace AddOn_DocGenerator.UL.Menus
         {
             try
             {
-                var app = SapConnection.GetApplication();
+                Application app = SapConnection.GetApplication();
 
-                string path = Path.Combine(
-                    AppDomain.CurrentDomain.BaseDirectory,
-                    "Resources",
-                    "Menus",
-                    "Menu.xml"
-                );
+                string menuPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Menus", "Menu.xml");
 
-                if (!File.Exists(path))
-                    throw new FileNotFoundException("No se encontró el archivo Menu.xml", path);
+                string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Images", "doc-gen-menu.bmp");
+
+                if (!File.Exists(menuPath))
+                    throw new FileNotFoundException("No se encontró el archivo Menu.xml", menuPath);
+
+                if (!File.Exists(iconPath))
+                    throw new FileNotFoundException("No se encontró el ícono doc-gen-menu.bmp", iconPath);
+
+                RemoveMenuIfExists(app, GENERATOR_MENU_UID);
+                RemoveMenuIfExists(app, MAIN_MENU_UID);
 
                 XmlDocument xml = new XmlDocument();
-                xml.Load(path);
+                xml.Load(menuPath);
+
+                XmlNode mainMenuNode = xml.SelectSingleNode($"//Menu[@UniqueID='{MAIN_MENU_UID}']");
+
+                if (mainMenuNode?.Attributes != null)
+                    mainMenuNode.Attributes["Image"].Value = iconPath;
 
                 app.LoadBatchActions(xml.InnerXml);
 
@@ -37,6 +48,21 @@ namespace AddOn_DocGenerator.UL.Menus
             catch (Exception ex)
             {
                 SapConnection.GetApplication().StatusBar.SetText($"Error cargando menú: {ex.Message}", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
+            }
+        }
+
+        /// <summary>
+        /// Elimina un menú de SAP Business One si ya existe, evitando errores por duplicidad al volver a cargarlo.
+        /// </summary>
+        private static void RemoveMenuIfExists(Application app, string menuUid)
+        {
+            try
+            {
+                app.Menus.Item(menuUid);
+                app.Menus.RemoveEx(menuUid);
+            }
+            catch
+            {
             }
         }
     }
