6cc035199e38341d69c0f22dac788b23ef3f5d0c
Franc02306 <francosm230602@gmail.com>
2026-06-13 12:25:20 -0500

    [feat] mapeo completo de CFL para OPRJ OPRC OWHS OITM seteando data en cada columna correspondiente

--- AddOn_DocGenerator.UL/Resources/FormClasses/UL_FrmMain.cs
+++ AddOn_DocGenerator.UL/Resources/FormClasses/UL_FrmMain.cs
@@ -17,12 +17,12 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
         private const string CRD_CFL_UID = "CFL_OCRD";
         private const string PRJ_CFL_UID = "CFL_OPRJ";
         private const string WHS_CFL_UID = "CFL_OWHS";
+        private const string ITM_CFL_UID = "CFL_OITM";
         private const string PRC_CFL_UID_1 = "CFL_OPRC1"; // Dimensión 1
         private const string PRC_CFL_UID_2 = "CFL_OPRC2"; // Dimensión 2
         private const string PRC_CFL_UID_3 = "CFL_OPRC3"; // Dimensión 3
         private const string PRC_CFL_UID_4 = "CFL_OPRC4"; // Dimensión 4
         private const string PRC_CFL_UID_5 = "CFL_OPRC5"; // Dimensión 5
-        private const string ITM_CFL_UID = "CFL_OITM";
 
         // COL
         public const string MATRIX_UID = "MtxDocs";
@@ -30,6 +30,9 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
         private const string COL_CARD_CODE = "ClmCodPrv";
         private const string COL_RUC = "ClmRucPrv";
         private const string COL_CARD_NAME = "ClmRznPrv";
+        private const string COL_WHS_CODE = "ClmCodWhs";
+        private const string COL_PRJ_CODE = "ClmCodPrj";
+        private const string COL_ITM_CODE = "ClmItmCde";
         private const string COL_DIM1 = "ClmDim1";
         private const string COL_DIM2 = "ClmDim2";
         private const string COL_DIM3 = "ClmDim3";
@@ -44,6 +47,8 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
         private const string FIELD_DOC_DATE = "U_DOCDATE";
         private const string FIELD_DUE_DATE = "U_DUEDATE";
         private const string FIELD_CURRENCY = "U_CURRENCY";
+        private const string FIELD_WHS_CODE = "U_WHS_CDE";
+        private const string FIELD_PRJ_CODE = "U_PRJ_CDE";
 
         #endregion
 
@@ -263,11 +268,28 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
         }
 
         /// <summary>
+        /// Configura el CFL de artículos para mostrar únicamente artículos activos.
+        /// </summary>
+        private static void ConfigureItemChooseFromList(Form form)
+        {
+            ChooseFromList itemCfl = form.ChooseFromLists.Item(ITM_CFL_UID);
+
+            Conditions conditions = new Conditions();
+
+            Condition condition = conditions.Add();
+            condition.Alias = "frozenFor";
+            condition.Operation = BoConditionOperation.co_EQUAL;
+            condition.CondVal = "N";
+
+            itemCfl.SetConditions(conditions);
+        }
+
+        /// <summary>
         /// Configura el CFL de centros de costo para mostrar únicamente los centros de costo activos.
         /// </summary>
-        private static void ConfigureCostCenterChooseFromList(Form form, string dimensionCode)
+        private static void ConfigureCostCenterChooseFromList(Form form, string cflUid, string dimensionCode)
         {
-            ChooseFromList costCenterCfl = form.ChooseFromLists.Item(PRC_CFL_UID_1);
+            ChooseFromList costCenterCfl = form.ChooseFromLists.Item(cflUid);
 
             Conditions conditions = new Conditions();
 
@@ -293,6 +315,9 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
             ConfigureCostCenterChooseFromListBeforeOpen(formUid, eventInfo);
             HandleSupplierChooseFromList(formUid, eventInfo);
             HandleCostCenterChooseFromList(formUid, eventInfo);
+            HandleWarehouseChooseFromList(formUid, eventInfo);
+            HandleProjectChooseFromList(formUid, eventInfo);
+            HandleItemChooseFromList(formUid, eventInfo);
         }
 
         /// <summary>
@@ -315,27 +340,33 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
             if (eventInfo.Row <= 0)
                 return;
 
+            string cflUid;
             string dimensionCode;
 
             switch (eventInfo.ColUID)
             {
                 case COL_DIM1:
+                    cflUid = PRC_CFL_UID_1;
                     dimensionCode = "1";
                     break;
 
                 case COL_DIM2:
+                    cflUid = PRC_CFL_UID_2;
                     dimensionCode = "2";
                     break;
 
                 case COL_DIM3:
+                    cflUid = PRC_CFL_UID_3;
                     dimensionCode = "3";
                     break;
 
                 case COL_DIM4:
+                    cflUid = PRC_CFL_UID_4;
                     dimensionCode = "4";
                     break;
 
                 case COL_DIM5:
+                    cflUid = PRC_CFL_UID_5;
                     dimensionCode = "5";
                     break;
 
@@ -346,7 +377,7 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
             Application app = SapConnection.GetApplication();
             Form form = app.Forms.Item(formUid);
 
-            ConfigureCostCenterChooseFromList(form, dimensionCode);
+            ConfigureCostCenterChooseFromList(form, cflUid, dimensionCode);
         }
 
         /// <summary>
@@ -416,6 +447,9 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
             }
         }
 
+        /// <summary>
+        /// Maneja la selección del centro de costo desde el CFL y lo coloca en la matrix.
+        /// </summary>
         private static void HandleCostCenterChooseFromList(string formUid, ItemEvent eventInfo)
         {
             if (formUid != FORM_UID)
@@ -481,6 +515,170 @@ namespace AddOn_DocGenerator.UL.Resources.FormClasses
             int dataSourceRow = eventInfo.Row - 1;
 
             detailDataSource.SetValue(fieldName, dataSourceRow, prcCode);
+
+            matrix.LoadFromDataSource();
+
+            RenumberMatrix(matrix);
+
+            matrix.FlushToDataSource();
+        }
+
+        /// <summary>
+        /// Maneja la selección del almacén desde el CFL y lo coloca en la matrix.
+        /// </summary>
+        private static void HandleWarehouseChooseFromList(string formUid, ItemEvent eventInfo)
+        {
+            if (formUid != FORM_UID)
+                return;
+
+            if (eventInfo.EventType != BoEventTypes.et_CHOOSE_FROM_LIST)
+                return;
+
+            if (eventInfo.BeforeAction)
+                return;
+
+            if (eventInfo.ItemUID != MATRIX_UID)
+                return;
+
+            if (eventInfo.ColUID != COL_WHS_CODE)
+                return;
+
+            if (eventInfo.Row <= 0)
+                return;
+
+            IChooseFromListEvent chooseFromListEvent = (IChooseFromListEvent)eventInfo;
+
+            if (chooseFromListEvent.ChooseFromListUID != WHS_CFL_UID)
+                return;
+
+            DataTable selectedObjects = chooseFromListEvent.SelectedObjects;
+
+            if (selectedObjects == null || selectedObjects.Rows.Count == 0)
+                return;
+
+            string whsCode = GetDataTableValue(selectedObjects, "WhsCode");
+
+            Application app = SapConnection.GetApplication();
+            Form form = app.Forms.Item(formUid);
+
+            Matrix matrix = (Matrix)form.Items.Item(MATRIX_UID).Specific;
+            DBDataSource detailDataSource = form.DataSources.DBDataSources.Item(DS_DETAIL);
+
+            matrix.FlushToDataSource();
+
+            int dataSourceRow = eventInfo.Row - 1;
+
+            detailDataSource.SetValue(FIELD_WHS_CODE, dataSourceRow, whsCode);
+
+            matrix.LoadFromDataSource();
+
+            RenumberMatrix(matrix);
+
+            matrix.FlushToDataSource();
+        }
+
+        /// <summary>
+        /// Maneja la selección del proyecto desde el CFL y lo coloca en la matrix.
+        /// </summary>
+        private static void HandleProjectChooseFromList(string formUid, ItemEvent eventInfo)
+        {
+            if (formUid != FORM_UID)
+                return;
+
+            if (eventInfo.EventType != BoEventTypes.et_CHOOSE_FROM_LIST)
+                return;
+
+            if (eventInfo.BeforeAction)
+                return;
+
+            if (eventInfo.ItemUID != MATRIX_UID)
+                return;
+
+            if (eventInfo.ColUID != COL_PRJ_CODE)
+                return;
+
+            if (eventInfo.Row <= 0)
+                return;
+
+            IChooseFromListEvent chooseFromListEvent = (IChooseFromListEvent)eventInfo;
+
+            if (chooseFromListEvent.ChooseFromListUID != PRJ_CFL_UID)
+                return;
+
+            DataTable selectedObjects = chooseFromListEvent.SelectedObjects;
+
+            if (selectedObjects == null || selectedObjects.Rows.Count == 0)
+                return;
+
+            string prjCode = GetDataTableValue(selectedObjects, "PrjCode");
+
+            Application app = SapConnection.GetApplication();
+            Form form = app.Forms.Item(formUid);
+
+            Matrix matrix = (Matrix)form.Items.Item(MATRIX_UID).Specific;
+            DBDataSource detailDataSource = form.DataSources.DBDataSources.Item(DS_DETAIL);
+
+            matrix.FlushToDataSource();
+
+            int dataSourceRow = eventInfo.Row - 1;
+
+            detailDataSource.SetValue(FIELD_PRJ_CODE, dataSourceRow, prjCode);
+
+            matrix.LoadFromDataSource();
+
+            RenumberMatrix(matrix);
+
+            matrix.FlushToDataSource();
+        }
+
+        /// <summary>
+        /// Maneja la selección del artículo desde el CFL y lo coloca en la matrix.
+        /// </summary>
+        private static void HandleItemChooseFromList(string formUid, ItemEvent eventInfo)
+        {
+            if (formUid != FORM_UID)
+                return;
+
+            if (eventInfo.EventType != BoEventTypes.et_CHOOSE_FROM_LIST)
+                return;
+
+            if (eventInfo.BeforeAction)
+                return;
+
+            if (eventInfo.ItemUID != MATRIX_UID)
+                return;
+
+            if (eventInfo.ColUID != COL_ITM_CODE)
+                return;
+
+            if (eventInfo.Row <= 0)
+                return;
+
+            IChooseFromListEvent chooseFromListEvent = (IChooseFromListEvent)eventInfo;
+
+            if (chooseFromListEvent.ChooseFromListUID != ITM_CFL_UID)
+                return;
+
+            DataTable selectedObjects = chooseFromListEvent.SelectedObjects;
+
+            if (selectedObjects == null || selectedObjects.Rows.Count == 0)
+                return;
+
+            string itemCode = GetDataTableValue(selectedObjects, "ItemCode");
+
+            Application app = SapConnection.GetApplication();
+            Form form = app.Forms.Item(formUid);
+
+            Matrix matrix = (Matrix)form.Items.Item(MATRIX_UID).Specific;
+            DBDataSource detailDataSource = form.DataSources.DBDataSources.Item(DS_DETAIL);
+
+            matrix.FlushToDataSource();
+
+            int dataSourceRow = eventInfo.Row - 1;
+
+            string fieldName = matrix.Columns.Item(COL_ITM_CODE).DataBind.Alias;
+
+            detailDataSource.SetValue(fieldName, dataSourceRow, itemCode);
 
             matrix.LoadFromDataSource();
 
